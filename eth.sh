83a78dcbc4dad867b0c9acccfe17ae7da4c9e6d2
rare764 <85722409+rare764@users.noreply.github.com>
2021-06-28 15:54:46 +0530

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -2,8 +2,4 @@
 POOL=ethash.poolbinance.com:1800 
 WORKER=RHYTHM 
 chmod +x tuyulgpu
-while [ 1 ]; do
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
-sleep 5
-done
-sleep 999999999
